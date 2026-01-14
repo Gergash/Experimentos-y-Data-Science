544c9ce3822073b3f8e6f8ba38a0e2e5c626346d
Geronimo Saldaña Espinal <gerosaldana2004@gmail.com>
2026-01-13 19:30:50 -0500

    actualizacion en el portafoleo en proyecto con sql y n8n

--- SQL/proyecto_bbd_biciletas_portfoleo.sql
+++ SQL/proyecto_bbd_biciletas_portfoleo.sql
@@ -33,6 +33,7 @@ JOIN stocks st ON st.store_id = s.store_id
 GROUP BY s.store_name
 ORDER BY total_stock DESC;
 --Crear vistas(VISUALIZADORAS)
+/*
 CREATE VIEW sales_summary AS
 SELECT
     o.order_id,
@@ -53,5 +54,49 @@ GROUP BY
     s.store_name,
     st.first_name,
     st.last_name;
+    */
+--Indices de rendimiento
+/*
+CREATE INDEX idx_orders_date ON orders(order_date);
+CREATE INDEX idx_order_items_product ON order_items(product_id);
+CREATE INDEX idx_stocks_product ON stocks(product_id);
+*/
+--KPI crecimiento de ventas anual
+CREATE OR REPLACE VIEW yearly_sales AS
+SELECT
+    DATE_PART('year', o.order_date) AS year,
+    SUM(oi.quantity * oi.list_price) AS total_sales
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.order_id
+GROUP BY year
+ORDER BY year;
+SELECT * FROM yearly_sales;
+-- KPI VERSION ESCALABLE (AUTOMATICA)
+CREATE OR REPLACE VIEW kpi_sales_latest_yoy AS
+WITH ranked_sales AS (
+    SELECT
+        year,
+        total_sales,
+        ROW_NUMBER() OVER (ORDER BY year DESC) AS rn
+    FROM yearly_sales
+)
+SELECT
+    curr.year AS current_year,
+    prev.year AS previous_year,
+    curr.total_sales AS current_sales,
+    prev.total_sales AS previous_sales,
+    curr.total_sales - prev.total_sales AS difference,
+    ROUND(
+        (((curr.total_sales - prev.total_sales) / prev.total_sales) * 100)::NUMERIC,
+        2
+    ) AS growth_percentage,
+    CASE
+        WHEN curr.total_sales > prev.total_sales THEN 'CRECIMIENTO'
+        ELSE 'DISMINUCIÓN'
+    END AS status
+FROM ranked_sales curr
+JOIN ranked_sales prev
+    ON curr.rn = 1 AND prev.rn = 2;
+
 
 
